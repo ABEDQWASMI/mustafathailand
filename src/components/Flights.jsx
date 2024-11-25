7f00dcd4217901ac5c4dfb043b480926a071c10b
ABEDQWASMI <abed@leapthelimit.com>
2024-11-25 12:55:18 +0200

    Fix map function error and update manifest.json

--- src/components/Flights.jsx
+++ src/components/Flights.jsx
@@ -1,10 +1,6 @@
 import { Heading, SimpleGrid, Box, Container } from "@chakra-ui/react";
 import React from "react";
 import "../styles/Flights.css";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Navigation } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
 
 const Flights = () => {
   const airlines = [
